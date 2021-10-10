e4678fc449fa1c00e8559839ae4e316e9f9345ec
planetdoy <doydoit@gmail.com>
2021-10-10 14:46:03 +0900

    feat : HATEOAS 적용

--- src/main/java/com/example/restapi/user/UserApiController.java
+++ src/main/java/com/example/restapi/user/UserApiController.java
@@ -1,12 +1,18 @@
 package com.example.restapi.user;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserApiController {
@@ -31,8 +37,31 @@ public class UserApiController {
             throw new UserNotFoundException(String.format("ID [%s] not found", id));
         }
 
+        //hateoas
+        EntityModel<User> entityModel = new EntityModel<>(user);
+
         return user;
     }
+
+
+    /**
+     * HATEOAS 적용
+     */
+    @GetMapping("/users2")
+    public ResponseEntity<CollectionModel<EntityModel<User>>> retrieveUserList2() {
+        List<EntityModel<User>> result = new ArrayList<>();
+        List<User> users = userDaoService.findAll();
+
+        for (User user : users) {
+            EntityModel entityModel = EntityModel.of(user);
+            entityModel.add(linkTo(methodOn(this.getClass()).users()).withSelfRel());
+
+            result.add(entityModel);
+        }
+
+        return ResponseEntity.ok(CollectionModel.of(result, linkTo(methodOn(this.getClass()).users()).withSelfRel()));
+    }
+
 
     @PostMapping("/users")
     public ResponseEntity<User> join(@Validated @RequestBody User user) {
